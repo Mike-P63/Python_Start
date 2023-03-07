12392edf0bcea3f9a619a3d3c18b478dc17204bb
Mike-P63 <rumaish@rambler.ru>
2023-03-08 00:04:46 +0400

    Made changes in main file

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+#m = int(input("Enter m = "))
+#n = int(input("Enter n = "))
+
+#print("Результат: " , (m//n)+1)
+
+#a = int(input("Enter a class pupils quantity = "))
+#b = int(input("Enter b class pupils quantity = "))
+#c = int(input("Enter c class pupils quantity = "))
+#print("Min tables quantity = ", ((a+b+c)//2)+1)
+
+#i = int(input("Enter i = "))
+#j = int(input("Enter j = "))
+#if i==j:
+    #print("Задача нерешаема")
+#else: print(i+j-1)
+
+#n = int(input("n = "))
+#print("YES" if n%4 == 0 and (n%100 != 0 or n%400 == 0) else "NO")
