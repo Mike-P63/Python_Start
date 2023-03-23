508f355a32ac36a527ea0fef70ce822f8c74bd99
Mike-P63 <rumaish@rambler.ru>
2023-03-24 00:09:47 +0400

    Added file  Main 3 from Seminar 5

---
+++ Main_3.py
@@ -0,0 +1,14 @@
+
+
+def rect(n,m):
+    s = n*m
+    return s
+def tri(h,m):
+    s = 0,5*h*m
+    return s
+def circl(r):
+    return 3.14*r**2
+
+# print(rect(2,3))
+# print(tri(2,3))
+# print(circl(3))
